ecc3f9c88cdb1164a795345a5d46c5fbf1699ab5
Alex Bihas <alex.bigas.2004@gmail.com>
2023-07-21 00:18:47 +0300

    initial pages structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import ProductPage from './pages/ProductPage';
+import PricingPage from './pages/PricingPage';
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<ProductPage />} />
+        <Route path="pricing" element={<PricingPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
