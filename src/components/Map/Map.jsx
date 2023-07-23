66479ed8791304e104b6a4e199377831b4a09a73
Alex Bihas <alex.bigas.2004@gmail.com>
2023-07-23 19:35:02 +0300

    implemented basic map functionality

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,19 +1,34 @@
 import React, { useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+import styles from './Map.module.css';
 
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
 
-import styles from './Map.module.css';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useCities } from '../../contexts/CitiesContext';
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+import L from 'leaflet';
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const Map = () => {
   const navigate = useNavigate();
+  const { cities } = useCities();
 
   const [searchParams, setSearchParams] = useSearchParams();
 
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
 
-  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const [mapPosition, setMapPosition] = useState([52, 13]);
 
   return (
     <div onClick={() => navigate('form')} className={styles.mapContainer}>
@@ -27,11 +42,11 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map(el => (
+          <Marker key={el.id} position={[el.position.lat, el.position.lng]}>
+            <Popup>{el.notes ? el.notes : `A Trip to ${el.cityName}`}</Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
